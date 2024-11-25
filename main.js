8888472a6681145f8faf6833377e9a57d7f5cb52
abram7777777 <abramashraf28@gmail.com>
2024-11-25 21:39:36 +0200

    second commit

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-
+var listt = [
+    {
+        quote : "“Be yourself; everyone else is already taken.”"
+        writer : "― Oscar Wilde"
+    },
+    {
+        quote : "“You only live once, but if you do it right, once is enough.”"
+        writer : "― Mae West"
+    },
+    {
+        quote : "“Be the change that you wish to see in the world.”"
+        writer : "― Mahatma Gandhi"
+    }
+]
 function shift(){
     var x =Math.floor(Math.random() * 3) + 1;
 if(x==1){
-    document.getElementById("container").innerHTML=`<p>“Be yourself; everyone else is already taken.”</p> <p>― Oscar Wilde</p>`;
+    document.getElementById("p1").innerHTML= listt[0].quote;
+    document.getElementById("p2").innerHTML= listt[0].writer;
 }else if(x==2){
-    document.getElementById("container").innerHTML=`<p>“You only live once, but if you do it right, once is enough.”</p> <p>― Mae West</p>`;
+    document.getElementById("p1").innerHTML= listt[1].quote;
+    document.getElementById("p2").innerHTML= listt[1].writer;
 }else{
-    document.getElementById("container").innerHTML=`<p>“Be the change that you wish to see in the world.”</p> <p>― Mahatma Gandhi</p>`;
+    document.getElementById("p1").innerHTML= listt[2].quote;
+    document.getElementById("p2").innerHTML= listt[2].writer;
 }
 }
